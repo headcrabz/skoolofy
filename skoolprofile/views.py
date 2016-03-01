30889e49c33c00a405f637c96c5bcd4466e7301f
anshul <anshulnsit@gmail.com>
2016-03-01 00:27:36 -0500

    new html

--- skoolprofile/views.py
+++ skoolprofile/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import school
+from .models import timezone
 
 # Create your views here.
 def school_list(request):
-    return render(request,'skoolprofile/school_list.html',{})
+    schools= school.objects.order_by('name')
+    return render(request,'skoolprofile/school_list.html',{'schools':schools})
